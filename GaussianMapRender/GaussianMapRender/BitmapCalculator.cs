6bff2cd9a0a5730070239f22d41d9b372a7b1bbb
tejaskb04 <tejaskb@gmail.com>
2018-03-30 23:28:08 -0700

    Painting Markers

---
+++ GaussianMapRender/GaussianMapRender/BitmapCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device;
+using System.Device.Location;
+
+namespace GaussianMapRender
+{
+    class BitmapCalculator
+    {
+        public BitmapCalculator()
+        {
+
+        }
+        public double calculateDistance(double startLat, double startLng, double endLat, double endLng)
+        {
+            GeoCoordinate coord_1 = new GeoCoordinate(startLat, startLng);
+            GeoCoordinate coord_2 = new GeoCoordinate(endLat, endLng);
+
+            return coord_1.GetDistanceTo(coord_2);
+        }
+        public double calculateBitmapWidth(double distance, double maxDistance, double imageWidth)
+        {
+            Console.WriteLine(distance + " " + maxDistance);
+            double ratio = distance / maxDistance;
+            Console.WriteLine("RATIO: " + ratio);
+            double width = imageWidth * ratio;
+            Console.WriteLine("WIDTH OF BITMAP: " + width);
+            return width;
+        }
+        public double calculateBitmapHeight(double distance, double maxDistance, double imageHeight)
+        {
+            double ratio = distance / maxDistance;
+            double height = imageHeight * ratio;
+            return height;
+        }
+    }
+}
--- GaussianMapRender/GaussianMapRender/Form1.cs
+++ GaussianMapRender/GaussianMapRender/Form1.cs
@@ -34,6 +34,9 @@ namespace GaussianMapRender
             gmap.SetPositionByKeywords(DEFAULT_LOCATION);
             gmap.ShowCenter = false;
             gmap.Zoom = 5;
+
+            // Debug Code
+            latLngToPixel();
         }
 
         private void gmap_Load(object sender, EventArgs e)
@@ -47,6 +50,65 @@ namespace GaussianMapRender
             Console.WriteLine(img.Width);
             Console.WriteLine(img.Height);
             return img;
+        }
+
+        public void renderBitmaps(List<double> lats, List<double> lngs, List<double> alphaValues, Image img)
+        {
+            Bitmap[,] preStitchedCollection = new Bitmap[lats.Count, lngs.Count];
+            int count = 0;
+            for (int i = 0; i < lats.Count; i++)
+            {
+                for (int j = 0; j < lngs.Count; j++)
+                {
+                    // add bitmap to collection for stitching process
+                    //preStitchedCollection[i, j] = getAlphaMap((int)alphaValues[count], width, height);
+                    count++;
+                }
+            }
+
+        }
+
+        public Bitmap getAlphaMap(double alphaValue, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bmp);
+            Color c = Color.FromArgb(((int)(alphaValue)), 255, 0, 0);
+            Brush b = new SolidBrush(c);
+            g.FillRectangle(b, 0, 0, width, height);
+            return bmp;
+        }
+
+        // TEST SCALE
+
+        public void latLngToPixel()
+        {
+            BitmapCalculator bc = new BitmapCalculator();
+            ParserManager P = new ParserManager();
+            P.execute();
+            List<double> lats = P.latitudeValues;
+            List<double> lngs = P.longitudeValues;
+            List<double> alphaValues = P.alphaValues;
+
+            // 1. Create the overlay
+            GMapOverlay markers = new GMapOverlay("markers");
+
+            for (int i =0; i < lats.Count; i++)
+            {
+                //Console.WriteLine(lats[i]);
+                for (int j = 0; j < lngs.Count; j++)
+                {
+                    //Console.WriteLine(lats[i] + " " + lngs[j]);
+                    PointLatLng point = new PointLatLng(lats[i], lngs[j]);
+                    GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red_pushpin);
+                    //marker.Position
+                    // 2. Add markers
+                    markers.Markers.Add(marker);
+
+                    // 3. Cover map with overlay
+                }
+            }
+            gmap.Overlays.Add(markers);
+
         }
     }
 }
